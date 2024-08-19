c866d87d4d138949ceb1b658ed1e380defbf3e9e
Luukas Pörtfors <lajp@iki.fi>
2024-08-19 23:07:59 +0300

    refactor: move user services to the correct location

---
+++ modules/user/services/default.nix
@@ -0,0 +1,19 @@
+{
+  config,
+  ...
+}: {
+  services = {
+    gpg-agent.enable = true;
+    mbsync.enable = true;
+
+    # FIXME: the repository still has to be initialized
+    # with git clone
+    git-sync = {
+      enable = true;
+      repositories.password-store = {
+        path = config.programs.password-store.settings.PASSWORD_STORE_DIR;
+        uri = "git@lajp.fi:/srv/git/pass.git";
+      };
+    };
+  };
+}
