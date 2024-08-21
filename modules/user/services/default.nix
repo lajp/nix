caaa7573947ea224232323974ad8a3cb45dcff28
Luukas Pörtfors <lajp@iki.fi>
2024-08-21 22:00:57 +0300

    chore: disable gpg-agent for now

--- modules/user/services/default.nix
+++ modules/user/services/default.nix
@@ -5,15 +5,15 @@
 }: {
   services = {
     #TODO: re-enable
-    gpg-agent = {
-      enable = true;
-      enableExtraSocket = true;
-      enableSshSupport = true;
-      pinentryPackage = pkgs.pinentry-gtk2;
-      extraConfig = ''
-        allow-loopback-pinentry
-      '';
-    };
+    #gpg-agent = {
+    #  enable = true;
+    #  enableExtraSocket = true;
+    #  enableSshSupport = true;
+    #  pinentryPackage = pkgs.pinentry-gtk2;
+    #  extraConfig = ''
+    #    allow-loopback-pinentry
+    #  '';
+    #};
 
     mbsync.enable = true;
 
