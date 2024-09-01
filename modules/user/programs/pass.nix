1a2826499c121e9aa7ffa89ad5b4ad2732d5facc
Luukas Pörtfors <lajp@iki.fi>
2024-09-01 13:42:11 +0300

    feat: add otp extension to pass

--- modules/user/programs/pass.nix
+++ modules/user/programs/pass.nix
@@ -1,8 +1,13 @@
-{config, ...}: let
+{
+  config,
+  pkgs,
+  ...
+}: let
   passDir = config.xdg.dataHome + "/password-store";
 in {
   programs.password-store = {
     enable = true;
+    package = pkgs.pass.withExtensions (exts: [exts.pass-otp]);
     settings.PASSWORD_STORE_DIR = passDir;
   };
 }
