447fb5c61c5720bf2a626b951523e48f1e8f2841
Luukas Pörtfors <lajp@iki.fi>
2024-09-01 13:39:40 +0300

    fix: use stylix for nixvim

--- modules/system/gui/theme.nix
+++ modules/system/gui/theme.nix
@@ -3,6 +3,5 @@
     enable = true;
     image = ../../../images/haskell.jpg;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
-    targets.nixvim.enable = false;
   };
 }
