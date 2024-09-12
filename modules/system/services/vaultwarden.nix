32cfebf7a4c5625d2cc5da62dda9fb5383d31fb9
Luukas Pörtfors <lajp@iki.fi>
2024-09-12 19:58:33 +0300

    feat: reverse-proxy vaultwarden

--- modules/system/services/vaultwarden.nix
+++ modules/system/services/vaultwarden.nix
@@ -8,6 +8,31 @@
 in {
   options.lajp.services.vaultwarden.enable = mkEnableOption "Enable vaultwarden";
   config = mkIf cfg.enable {
-    services.vaultwarden.enable = true;
+    services.vaultwarden = {
+      enable = true;
+      config = {
+        ROCKET_ADDRESS = "127.0.0.1";
+      };
+    };
+
+    services.nginx = {
+      enable = true;
+      recommendedProxySettings = true;
+      recommendedTlsSettings = true;
+
+      virtualHosts."vaultwarden.lajp.fi" = {
+        locations."/" = {
+          proxyPass = "http://localhost:${config.service.vaultwarden.ROCKET_PORT}"
+          proxyWebsockets = true;
+        };
+      };
+    };
+
+    security.acme = {
+      acceptTerms = true;
+      defaults.email = "lajp@iki.fi";
+    };
+
+    networking.firewall.allowedTCPPorts = [80 443];
   };
 }
