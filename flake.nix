1e6596853ca8d547f399c3adc7a8cf42738395d4
Luukas Pörtfors <lajp@iki.fi>
2024-11-05 15:32:12 +0200

    chore: flake update

--- flake.nix
+++ flake.nix
@@ -25,7 +25,7 @@
     };
 
     stylix = {
-      url = "github:danth/stylix";
+      url = "github:danth/stylix/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.home-manager.follows = "home-manager";
     };
