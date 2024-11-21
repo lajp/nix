5a2a270e5245948c5fbce870e4aa9a1ed9a8173d
Luukas Pörtfors <lajp@iki.fi>
2024-11-21 22:00:40 +0200

    fix(firefox): make firefox the default browser

--- modules/user/programs/firefox.nix
+++ modules/user/programs/firefox.nix
@@ -7,6 +7,19 @@
     inputs.nur.hmModules.nur
   ];
 
+  xdg.mimeApps.defaultApplications = {
+    "application/x-extension-htm" = "firefox.desktop";
+    "application/x-extension-html" = "firefox.desktop";
+    "application/x-extension-shtml" = "firefox.desktop";
+    "application/x-extension-xht" = "firefox.desktop";
+    "application/x-extension-xhtml" = "firefox.desktop";
+    "application/xhtml+xml" = "firefox.desktop";
+    "text/html" = "firefox.desktop";
+    "x-scheme-handler/chrome" = "firefox.desktop";
+    "x-scheme-handler/http" = "firefox.desktop";
+    "x-scheme-handler/https" = "firefox.desktop";
+  };
+
   programs.firefox = {
     enable = true;
     nativeMessagingHosts = [pkgs.web-eid-app];
