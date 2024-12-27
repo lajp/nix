ffa7bab50e11b359e5789f40667b3444fb1f5785
Luukas Pörtfors <lajp@iki.fi>
2024-12-27 22:09:23 +0200

    fix(podman): enable docker compat options

--- modules/system/virtualisation/podman.nix
+++ modules/system/virtualisation/podman.nix
@@ -9,13 +9,16 @@
 in {
   options.lajp.virtualisation.podman.enable = mkEnableOption "Enable Podman";
   config = mkIf cfg.enable {
+    virtualisation.containers.enable = true;
     virtualisation.podman = {
       enable = true;
       autoPrune.enable = true;
+      dockerSocket.enable = true;
+      dockerCompat = true;
       defaultNetwork.settings.dns_enabled = true;
       extraPackages = mkIf config.lajp.hardware.zfs.enable [pkgs.zfs];
     };
 
-    environment.systemPackages = mkIf (!config.lajp.core.server) [pkgs.podman-compose];
+    environment.systemPackages = [pkgs.docker-compose];
   };
 }
