c0d4cf9917cf31d22b2bb154f9ff8505dae04c31
Luukas Pörtfors <lajp@iki.fi>
2026-04-08 13:14:19 +0300

    chore: add more services to gatus

--- modules/system/services/gatus.nix
+++ modules/system/services/gatus.nix
@@ -104,6 +104,108 @@ in
               alerts = [ { type = "email"; } ];
             }
             {
+              name = "Synapse status";
+              group = "public";
+              url = "https://matrix.lajp.fi/_matrix/client/versions";
+              conditions = [
+                "[STATUS] == 200"
+                "[CERTIFICATE_EXPIRATION] > 48h"
+              ];
+              alerts = [ { type = "email"; } ];
+            }
+            {
+              name = "Hedgedoc";
+              group = "public";
+              url = "https://pad.lajp.fi/status";
+              conditions = [
+                "[STATUS] == 200"
+                "[CERTIFICATE_EXPIRATION] > 48h"
+              ];
+              alerts = [ { type = "email"; } ];
+            }
+            {
+              name = "Headscale";
+              group = "public";
+              url = "https://headscale.lajp.fi/health";
+              conditions = [
+                "[STATUS] == 200"
+                "[CERTIFICATE_EXPIRATION] > 48h"
+              ];
+              alerts = [ { type = "email"; } ];
+            }
+            {
+              name = "Element";
+              group = "public";
+              url = "https://element.lajp.fi/";
+              conditions = [
+                "[STATUS] == 200"
+                "[CERTIFICATE_EXPIRATION] > 48h"
+              ];
+              alerts = [ { type = "email"; } ];
+            }
+            {
+              name = "Grafana";
+              group = "public";
+              url = "https://grafana.lajp.fi/api/health";
+              conditions = [
+                "[STATUS] == 200"
+                "[CERTIFICATE_EXPIRATION] > 48h"
+              ];
+              alerts = [ { type = "email"; } ];
+            }
+            {
+              name = "Attic";
+              group = "public";
+              url = "https://cache.lajp.fi/";
+              conditions = [
+                "[STATUS] == 200"
+                "[CERTIFICATE_EXPIRATION] > 48h"
+              ];
+              alerts = [ { type = "email"; } ];
+            }
+            {
+              name = "CHEESE";
+              group = "public";
+              url = "https://cheese.lajp.fi/";
+              conditions = [
+                "[STATUS] == 200"
+                "[CERTIFICATE_EXPIRATION] > 48h"
+              ];
+              alerts = [ { type = "email"; } ];
+            }
+            {
+              name = "ESN iCal";
+              group = "public";
+              url = "https://esn-ical.lajp.fi/";
+              conditions = [
+                "[STATUS] == 200"
+                "[CERTIFICATE_EXPIRATION] > 48h"
+              ];
+              alerts = [ { type = "email"; } ];
+            }
+            {
+              name = "luuk.as";
+              group = "public";
+              url = "https://luuk.as/";
+              conditions = [
+                "[STATUS] == 200"
+                "[CERTIFICATE_EXPIRATION] > 48h"
+              ];
+              alerts = [ { type = "email"; } ];
+            }
+            {
+              name = "IMAP";
+              group = "public";
+              url = "tls://mail.portfo.rs:993";
+              interval = "3m";
+              client.timeout = "5s";
+              conditions = [
+                "[CONNECTED] == true"
+                "[CERTIFICATE_EXPIRATION] > 48h"
+              ];
+              alerts = [ { type = "email"; } ];
+            }
+            {
               name = "Vaultwarden";
               group = "internal";
               url = "https://vault.intra.lajp.fi/api/alive";
@@ -133,7 +235,6 @@ in
               ];
               alerts = [ { type = "email"; } ];
             }
-            # TODO: add nixarr services
             {
               name = "sonarr";
               group = "internal";
@@ -167,6 +268,53 @@ in
               url = "http://100.64.0.2:6767";
               conditions = [
                 "[STATUS] == 200"
+              ];
+              alerts = [ { type = "email"; } ];
+            }
+            {
+              name = "lidarr";
+              group = "internal";
+              url = "http://100.64.0.2:8686/ping";
+              conditions = [
+                "[STATUS] == 200"
+              ];
+              alerts = [ { type = "email"; } ];
+            }
+            {
+              name = "AdGuard Home";
+              group = "internal";
+              url = "https://adguard.intra.lajp.fi/";
+              conditions = [
+                "[STATUS] == 200"
+                "[CERTIFICATE_EXPIRATION] > 48h"
+              ];
+              alerts = [ { type = "email"; } ];
+            }
+            {
+              name = "AdGuard DNS";
+              group = "internal";
+              url = "100.64.0.3";
+              interval = "3m";
+              dns = {
+                query-name = "lajp.fi";
+                query-type = "A";
+              };
+              conditions = [
+                "[DNS_RCODE] == NOERROR"
+              ];
+              alerts = [ { type = "email"; } ];
+            }
+            {
+              name = "CoreDNS";
+              group = "internal";
+              url = "100.64.0.4";
+              interval = "3m";
+              dns = {
+                query-name = "lajp.fi";
+                query-type = "A";
+              };
+              conditions = [
+                "[DNS_RCODE] == NOERROR"
               ];
               alerts = [ { type = "email"; } ];
             }
